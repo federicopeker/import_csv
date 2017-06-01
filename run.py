f91968b650af8da467c83cae8e46848d8a767873
federicopeker <federicopeker@gmail.com>
2017-06-01 12:57:54 -0300

    add run

---
+++ run.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from parser import ParserCsv
+
+
+def main():
+    parser = ParserCsv()
+    print "normal"
+
+if __name__ == "__main__":
+    main()
